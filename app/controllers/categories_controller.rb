e2756d4ab661a8fe083ed1cccba01bc9e0e76279
codedit334 <oussama7elabdioui@gmail.com>
2023-07-08 16:07:36 +0100

    Add edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,6 +19,21 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+    # Render the edit view template
+    render :edit
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Category updated successfully.'
+    else
+      render :edit
+    end
+  end
+
   def destroy
     category = Category.find(params[:id])
     category.destroy
